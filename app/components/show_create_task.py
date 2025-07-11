5c242d793c3f46399a4523984c67729665bb54ad
Cesar <cesar.plyed@gmail.com>
2025-07-10 19:38:35 -0600

    Fix(show_create_task): The button 'Vaciar campos' works

--- app/components/show_create_task.py
+++ app/components/show_create_task.py
@@ -32,7 +32,7 @@ def show_create_task(w: tk.Tk, place: tk.Frame, column, row, padx=0, pady=0, row
     fromDateEntry = DateEntry(createTask, width=20, font=("Arial", 14), highlightcolor="green", highlightbackground="red", highlightthickness=1, border=0, date_pattern='yyyy-mm-dd')
     fromDateEntry.grid(column=2, row=1, padx=(5, 10), pady=(0, 10), sticky="we")
 
-    tk.Button(createTask, text="Vaciar campos", font=("Arial", 14), bg="#833131", fg="white", border=0 ).grid(column=3, row=1, padx=(5, 10), pady=(0, 10), sticky="", ipadx=10)
+    tk.Button(createTask, text="Vaciar campos", font=("Arial", 14), bg="#833131", fg="white", border=0, command=lambda: task.delete(0, tk.END)).grid(column=3, row=1, padx=(5, 10), pady=(0, 10), sticky="", ipadx=10)
     tk.Button(createTask, text="Crear tarea", font=("Arial", 14), bg="#4CAF50", fg="white", border=0, command=lambda: create_task_service(task.get(), toDateEntry.get(), fromDateEntry.get()) and upd_table(place)).grid(column=4, row=1, padx=(5, 10), pady=(0, 10), sticky="", ipadx=10)
 
     return createTask
